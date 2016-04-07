6b3d932234c6cab3a06cfa24a160ce4473d8b1a7
Timothée Poisot <tim@poisotlab.io>
2016-04-07 11:59:22 -0400

    Update to the use case 1 -- dynamics

--- scripts/usecase1.jl
+++ scripts/usecase1.jl
@@ -1,58 +1,53 @@
 using befwm
 using Gadfly
 using DataFrames
-using ProgressMeter
 
-# Generates a random network based on the niche model
-function nm(n, co)
-    A = nichemodel(n, co)
-    while round(befwm.connectance(A), 2) != co
-        A = nichemodel(n, co)
+
+steps = 500
+replicates = 5
+
+df = DataFrame([Int64, Int64, Float64, Float64], [:time, :replicate, :diversity, :biomass], (steps+1) * replicates)
+
+for i in 1:replicates
+    A = nichemodel(20, 0.25)
+    while abs(befwm.connectance(A) - 0.25) > 0.01
+        A = nichemodel(20, 0.25)
     end
-    return A
-end
-
-# Create a DataFrame with conditions
-replicates = 20
-df = DataFrame([Float64, Float64, Float64, Float64], [:Z, :Co, :stability, :diversity], length(Z)*replicates)
-index = 1
-for z in logspace(-1, 1, 5)
-    for co in linspace(0.05, 0.25, 10)
-        for re in 1:replicates
-            df[:Z][index] = z
-            df[:Co][index] = co
-            index += 1
-        end
-    end
-end
-
-df_index = 1
-progbar = Progress(length(Z)*replicates, 1, "Simulating ", 50)
-for i in eachindex(Z)
-    for s in 1:replicates
-        A = nm(20, 0.15)
-        p = make_initial_parameters(A)
-        p[:Z] = Z[i]
-        p = make_parameters(p)
-        initial_biomass = rand(size(A, 1))
-        output = simulate(p, initial_biomass, start=0, stop=2000, steps=2000, use=solver)
-        df[:Z][df_index] = Z[i]
-        df[:stability][df_index] = population_stability(output, last=1000, threshold=-0.01)
-        df[:diversity][df_index] = foodweb_diversity(output, last=1000)
-        df_index += 1
-        next!(progbar)
+    # Prepare the simulation parameters
+    p = make_initial_parameters(A)
+    p = make_parameters(p)
+    bm = rand(size(A, 1))
+    # Simulate!
+    out = simulate(p, bm, start=0, stop=steps, steps=1000, use=:ode45)
+    t = out[:t]
+    d = mapslices(befwm.shannon, out[:B], 2)
+    b = sum(out[:B], 2)
+    for time in eachindex(t)
+        index = (i-1)*(steps+1)+time
+        df[:time][index] = t[time]
+        df[:replicate][index] = i
+        df[:diversity][index] = d[time]
+        df[:biomass][index] = b[time]
     end
 end
 
-df = df[df[:stability].<0.0,:]
+# Melt data frame
+for_plot = by(df, :time,
+                x -> DataFrame(
+                    diversity = mean(x[:diversity]),
+                    s_diversity = std(x[:diversity]),
+                    biomass = mean(x[:biomass]),
+                    s_biomass = std(x[:biomass]),
+                    ))
 
-pub_theme = Theme(
-    panel_stroke = colorant"black",
-    grid_color = colorant"black",
-    highlight_width=0mm,
-)
+for_plot[:min_diversity] = for_plot[:diversity] .- for_plot[:s_diversity]
+for_plot[:max_diversity] = for_plot[:diversity] .+ for_plot[:s_diversity]
+for_plot[:min_biomass] = for_plot[:biomass] .- for_plot[:s_biomass]
+for_plot[:max_biomass] = for_plot[:biomass] .+ for_plot[:s_biomass]
 
-pl_stab = plot(df, x=:Z, y=:stability, Scale.x_log10, Geom.point, pub_theme)
-pl_even = plot(df, x=:Z, y=:diversity, Scale.x_log10, Geom.point, pub_theme)
+pl_div = plot(for_plot, x=:time, y=:diversity, ymin=:min_diversity, ymax=:max_diversity,
+    Geom.path, Geom.ribbon);
+pl_bio = plot(for_plot, x=:time, y=:biomass, ymin=:min_biomass, ymax=:max_biomass,
+    Geom.path, Geom.ribbon);
 
-draw(PDF("effect_scaling.pdf", 19cm, 9cm), hstack(pl_stab, pl_even))
+draw(PDF("temporal_dynamics", 12cm, 6cm), vstack(pl_div, pl_bio))
