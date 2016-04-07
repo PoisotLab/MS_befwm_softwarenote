7560015be914d21f12f15d51fd447e7933458944
Timothée Poisot <tim@poisotlab.io>
2016-04-07 12:28:49 -0400

    Use case 1 -- done

--- scripts/usecase1.jl
+++ scripts/usecase1.jl
@@ -4,7 +4,7 @@ using DataFrames
 
 
 steps = 500
-replicates = 5
+replicates = 50
 
 df = DataFrame([Int64, Int64, Float64, Float64], [:time, :replicate, :diversity, :biomass], (steps+1) * replicates)
 
@@ -18,7 +18,7 @@ for i in 1:replicates
     p = make_parameters(p)
     bm = rand(size(A, 1))
     # Simulate!
-    out = simulate(p, bm, start=0, stop=steps, steps=1000, use=:ode45)
+    out = simulate(p, bm, start=0, stop=steps, steps=1500, use=:ode45)
     t = out[:t]
     d = mapslices(befwm.shannon, out[:B], 2)
     b = sum(out[:B], 2)
@@ -32,22 +32,20 @@ for i in 1:replicates
 end
 
 # Melt data frame
-for_plot = by(df, :time,
-                x -> DataFrame(
-                    diversity = mean(x[:diversity]),
-                    s_diversity = std(x[:diversity]),
-                    biomass = mean(x[:biomass]),
-                    s_biomass = std(x[:biomass]),
-                    ))
 
-for_plot[:min_diversity] = for_plot[:diversity] .- for_plot[:s_diversity]
-for_plot[:max_diversity] = for_plot[:diversity] .+ for_plot[:s_diversity]
-for_plot[:min_biomass] = for_plot[:biomass] .- for_plot[:s_biomass]
-for_plot[:max_biomass] = for_plot[:biomass] .+ for_plot[:s_biomass]
+function distrmax(x)
+    return mean(x) .+ std(x)
+end
 
-pl_div = plot(for_plot, x=:time, y=:diversity, ymin=:min_diversity, ymax=:max_diversity,
+function distrmin(x)
+    return mean(x) .- std(x)
+end
+
+for_plot = aggregate(df, :time, [mean, std, distrmin, distrmax])
+
+pl_bio = plot(for_plot, x=:time, y=:biomass_mean, ymin=:biomass_distrmin, ymax=:biomass_distrmax,
     Geom.path, Geom.ribbon);
-pl_bio = plot(for_plot, x=:time, y=:biomass, ymin=:min_biomass, ymax=:max_biomass,
+pl_div = plot(for_plot, x=:time, y=:diversity_mean, ymin=:diversity_distrmin, ymax=:diversity_distrmax,
     Geom.path, Geom.ribbon);
 
-draw(PDF("temporal_dynamics", 12cm, 6cm), vstack(pl_div, pl_bio))
+draw(PDF("temporal_dynamics.pdf", 14cm, 17cm), vstack(pl_div, pl_bio))
