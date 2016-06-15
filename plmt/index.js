3c0c281e270f82a3e0d2603bbba0af4931b7b6ac
Timothée Poisot <tim@poisotlab.io>
2016-06-15 14:45:24 -0400

    default.json

--- plmt/index.js
+++ plmt/index.js
@@ -1,4 +1,4 @@
-var hylla = require('/home/tpoisot/Code/packages/node/hylla/hylla.js');
+var hylla = require('/home/tpoisot/code/packages/node/hylla/hylla.js');
 var fs = require('fs');
 var path = require('path');
 
