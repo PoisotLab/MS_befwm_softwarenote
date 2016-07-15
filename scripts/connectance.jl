689f02a82f631d61cab1434ddb045bc4d1cdf8f6
Timothée Poisot <tim@poisotlab.io>
2016-07-14 22:17:27 -0400

    add a parallel version of the connectance / competition code

--- scripts/connectance.jl
+++ scripts/connectance.jl
@@ -1,47 +1,49 @@
 include("common.jl")
 
-replicates = 10
-competition = linspace(0.9, 1.1, 11)
-connectance = linspace(0.05, 0.25, 5)
+@everywhere using befwm
 
-n = replicates * length(competition) * length(connectance)
+@everywhere competition = linspace(0.8, 1.2, 5)
+@everywhere connectance = linspace(0.05, 0.25, 3)
 
-df = DataFrame([Float64, Float64, Float64, Float64, Float64, Float64], [:competition, :connectance, :diversity, :stability, :richness, :biomass], n)
+@everywhere conditions = vcat([[[con, com] for com in competition] for con in connectance]...)
 
-i = 1
-
-for replicate in 1:replicates
-  for co in connectance
-    for α in competition
-      println("Sim. $i of $n")
-      # Generate a niche model
-      A = nichemodel(20, co)
-      while abs(befwm.connectance(A) - co) > 0.01
-        A = nichemodel(20, co)
-      end
-      # Simulate
-      p = model_parameters(A, productivity=:competitive, α=α)
-      bm = rand(size(A, 1))
-      out = simulate(p, bm, start=0, stop=2000, use=:ode45)
-      # Get results
-      d = foodweb_diversity(out, last=1000)
-      s = population_stability(out, last=1000, threshold=eps())
-      b = total_biomass(out, last=1000)
-      r = species_richness(out, last=1000, threshold=eps())
-      # Add to data frame
-      df[:competition][i] = p[:α]
-      df[:connectance][i] = co
-      df[:diversity][i] = d
-      df[:stability][i] = s
-      df[:biomass][i] = b
-      df[:richness][i] = r
-      # Update cursor
-      i += 1
-    end
+@everywhere function makesim(co, α)
+  # Generate a niche model
+  A = nichemodel(20, co)
+  while abs(befwm.connectance(A) - co) > 0.01
+    A = nichemodel(20, co)
   end
+  # Simulate
+  p = model_parameters(A, productivity=:competitive, α=α)
+  bm = rand(size(A, 1))
+  out = simulate(p, bm, start=0, stop=2000, use=:ode45)
+  # Get results
+  d = foodweb_diversity(out, last=1000)
+  s = population_stability(out, last=1000, threshold=-0.001)
+  b = total_biomass(out, last=1000)
+  r = species_richness(out, last=1000, threshold=eps()) / 20.0
+  # Return
+  return (d, s, b, r)
 end
 
-plot(df, x=:competition, y=:richness, colour=:connectance, Geom.point, Geom.smooth, Scale.color_discrete())
+
+replicates = 5
+df = DataFrame([Float64, Float64, Float64, Float64, Float64, Float64], [:competition, :connectance, :diversity, :stability, :richness, :biomass], replicates * length(conditions))
+
+cursor = 1
+for replicate in 1:replicates
+  println("Starting replicate $replicate")
+  output = pmap((x) -> makesim(x...), conditions)
+  for k in eachindex(output)
+    df[:competition][cursor] = conditions[k][2]
+    df[:connectance][cursor] = conditions[k][1]
+    df[:diversity][cursor] = output[k][1]
+    df[:stability][cursor] = output[k][2]
+    df[:biomass][cursor] = output[k][3]
+    df[:richness][cursor] = output[k][4]
+    cursor += 1
+  end
+end
 
 # Filter results
 df = df[!isnan(df[:diversity]),:]
